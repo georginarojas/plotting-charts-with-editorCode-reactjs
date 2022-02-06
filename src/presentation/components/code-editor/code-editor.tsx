f6adadefc2ac0f196aa9f3aed0fe6b0e118b481f
georgina <georgina.rojas@gentrop.com>
2022-02-06 20:15:50 -0300

    feat: create CodeEditor component

---
+++ src/presentation/components/code-editor/code-editor.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import AceEditor from 'react-ace'
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/theme-monokai'
+
+type Props = {
+  rawCode: any
+  handleChange: (value: any) => void
+}
+
+const CodeEditor: React.FC<Props> = ({ rawCode, handleChange }) => {
+  return (
+    <AceEditor
+      mode="javascript"
+      theme="monokai"
+      value={rawCode}
+      onChange={(newValue) => {
+        handleChange(newValue)
+      }}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{ $blockScrolling: true }}
+      setOptions={{
+        // tabSize: 2,
+        showLineNumbers: true
+      }}
+      style={{
+        width: '100%',
+        height: '200px'
+      }}
+      fontSize={16}
+    />
+  )
+}
+
+export default CodeEditor
